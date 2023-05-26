3860b161933b8a9f669c451c0fe25d50305bd3ef
Flipped <flipped@flipped199.com>
2023-05-26 20:27:34 +0800

    add response.go

---
+++ response.go
@@ -0,0 +1,21 @@
+package go_zero_common_response
+
+import "net/http"
+
+type Resp struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+	Data any    `json:"data,omitempty"`
+}
+
+func Response(w http.ResponseWriter, resp any, err error) {
+	var r Resp
+	if err != nil {
+		r.Code = -1
+		r.Msg = err.Error()
+	} else {
+		r.Msg = "Success"
+		r.Data = resp
+	}
+	httpx.OkJson(w, r)
+}
